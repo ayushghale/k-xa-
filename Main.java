b9924c4f77f5d1c66ee05d15e898f8d330445beb
ayushghale <ghaleayush03@gmail.com>
2022-02-22 12:42:03 +0545

    fist git

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class MyClass
+{
+    public static void mian(String[] args) {
+        int a= 10;
+        String name ="Test";
+        int total =a+10;
+        System.out.println("Name :"+name);
+        System.out.println("total :"+total);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class New1{
+    public static void mian(String[] args) {
+        int a= 10;
+        int b= 10;
+        int total =a+b;
+        System.out.println("total :"+total);
+    }
+}
